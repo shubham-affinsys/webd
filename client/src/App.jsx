d9ea1d8cf4121c48ab9c34295c0a3b74a020bcee
shubham-affinsys <shubham.chuahan@affinssys.com>
2024-07-17 13:09:10 +0530

    july17

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,19 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count,setCount] = useState(0);
+
+  function handleClick(){
+    setCount(count+1);
+  }
 
   return (
-    <>
+    <div>
     <p>New app to learn django</p>
-    </>
+    <button onClick={handleClick}> count {count} </button>
+    </div>
   )
 }
 
-export default App
+export default App;
